ac88d9ea8a067faf0a894d64f8be70ea25ceb83a
unknown <chayapriyadarshani1@gmail.com>
2020-11-30 19:15:32 +0530

    Added on delete cascade reference for org_id field

--- database/migrations/2020_10_11_142429_create_organization_contacts_table.php
+++ database/migrations/2020_10_11_142429_create_organization_contacts_table.php
@@ -15,13 +15,14 @@ class CreateOrganizationContactsTable extends Migration
     {
         Schema::create('organization_contacts', function (Blueprint $table) {
             $table->id();
-            $table->integer('org_id');
+            $table->unsignedBigInteger('org_id');
             $table->string('type');  //fax, mobile, direct no, address, email
             $table->string('contact_signature'); //number, email string, address string
             $table->boolean('primary');
             $table->timestampsTz(); //time stamp with timezone in UTC
             $table->tinyInteger('status');
             $table->softDeletesTz('deleted_at', 0);
+            $table->foreign('org_id')->references('id')->on('organizations')->onDelete('cascade');
         });
     }
 
