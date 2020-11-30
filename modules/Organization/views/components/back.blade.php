c8892d0cb6693d2d9e1001b47ac6644e2027e784
unknown <chayapriyadarshani1@gmail.com>
2020-11-30 20:02:10 +0530

    blade for go to previous

--- modules/Organization/views/components/back.blade.php
+++ modules/Organization/views/components/back.blade.php
@@ -1,1 +1,3 @@
-<kbd><a href={{$url}} class="text-white font-weight-bolder"><i class="fas fa-chevron-left"></i></i> BACK</a></kbd>
+<kbd><a href={{$url}} class="text-white font-weight-bolder">
+    <i class="fas fa-chevron-left"></i></i> BACK</a>
+</kbd>
