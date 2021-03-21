b54d11fa9311943a27894d0bc028cd3219a9f217
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2021-03-21 13:56:48 +0530

    fixed the UI of the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,186 +23,159 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link href="{{ url('/css/app.css') }}" rel="stylesheet">
-    <style>
-        .inline {
-            display: inline;
-        }
-
-        .remove-all-margin {
-            margin: 0 ! important;
-        }
-
-        .container {
-            padding: 0px;
-        }
-
-        .input-group>.input-group-prepend {
-            flex: 0 0 30%;
-        }
-
-        .input-group .input-group-text {
-            width: 100%;
-        }
-
-        html {
-            position: relative;
-            min-height: 100%;
-        }
-
-        body {
-            margin-bottom: 60px;
-        }
-
-        .footer {
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-            height: 75px;
-            line-height: 15px;
-        }
-
-
-        /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
-    </style>
 </head>
 
-<body class="bg-secondary">
+<body style="background-color:#f0f0f7">
     @livewireScripts
-    <nav class="navbar navbar-expand-sm bd-navbar navbar-dark">
-        <!-- Brand/logo -->
-        <a class="navbar-brand mr-auto" href="#">
-            <img src="/Logo.jpeg" alt="logo" style="width:70px;">
-        </a>
-
-        <a href="#" class="text-secondary mr-1">Help |</a>
-
-
-        
-        <span>
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <i class="fas fa-bell mr-1"></i>
-                @if(auth()->user()->unreadNotifications->count())
-                <span class="badge badge-light">{{auth()->user()->unreadNotifications->count()}}</span> 
-                @endif
-            </a>
-
-            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <li><a href="/markAsRead" style="color:green"> Mark All As Read</a></li>
-                    @foreach(auth()->user()->unreadNotifications as $notification)
-                    <li>ID:{{$notification->data['id']}} - Type:{{$notification->data['type']}}</li>
-                    @endforeach
-                </ul>
-        </li>
-        </span>
-
-
-
-        <a href="/admin/passwordReset" class="text-secondary">
-            <i class="fas fa-cog mr-1"> |</i>
-        </a>
-
-
-        <ul class="navbar-nav">
-            <!-- Authentication Links -->
-            @guest
-            <li class="nav-item">
-                <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-            <li class="nav-item">
-                <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            @endif
-            @else
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-            @endguest
-        </ul>
-    </nav>
-
-
-
-    <br>
-
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-2">
-                <!--if screen size goes below large then start stacking-->
-                <div class="p-2 border bg-dark border-dark rounded-lg flex-shrink-3">
-                    <!-- A vertical navbar -->
-                    <nav class="navbar bg-dark">
+            <div class="col-lg-2 bd-navbar">
+                <div class="p-2 flex-shrink-3">
+                    <nav class="navbar">
                         <!-- Links -->
-                        <ul class="navbar-nav nav-tabs">
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="/general/pending">General Module</a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item mt-5">
+                                <a class="nav-link text-light" href="/general/pending"><i class="fa fa-home" aria-hidden="true"></i>  General Module</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="/user/index">User Management</a>
+                            <li class="nav-item mt-3">
+                                <a class="nav-link text-light" href="/user/index"><i class="fa fa-user" aria-hidden="true"></i>  User Management</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="/environment/generalenv">Environment Module</a>
+                            <li class="nav-item mt-3">
+                                <a class="nav-link text-light" href="/environment/generalenv"><i class="fa fa-tree" aria-hidden="true"></i>  Environment Module</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="/organization/index">Admin Module</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="#">Security Module</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="#">Update Map</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light font-italic p-2" href="/approval-item/showRequests">Requests</a>
+                            <li class="nav-item mt-3">
+                                <a class="nav-link text-light" href="/approval-item/showRequests"><i class="fa fa-arrow-down" aria-hidden="true"></i>  Requests</a>
                             </li>
                         </ul>
                     </nav>
                 </div>
             </div>
-            <div style="background-color:#f0f0f7" class="col-md p-2 border border-secondary rounded-lg ml-2 mr-3">
-                @yield('cont')
+            <div class="col-lg">
+
+                <!-- TOP NAV -->
+
+                <nav class="navbar navbar-expand-sm bd-navbar navbar-dark">
+                    <!-- Brand/logo -->
+                    <a class="navbar-brand mr-auto" href="#">
+                        <img src="/Logo.jpeg" alt="logo" style="width:70px;">
+                    </a>
+
+                    <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div class="media align-items-center">
+                            <div class="media-body  ml-2  d-none d-lg-block">
+                                <span class="mb-0 text-sm  text-white font-weight-bold"><i class="fas fa-bell mr-1"></i></span>
+                                @if(auth()->user()->unreadNotifications->count())
+                                <span class="mb-0 text-sm  font-weight-bold"><span class="badge badge-light">{{auth()->user()->unreadNotifications->count()}}</span></span>
+                                @endif
+                            </div>
+                        </div>
+                    </a>
+                    <div class="dropdown-menu  dropdown-menu-right ">
+                        <div class="dropdown-header noti-title">
+                            <h6 class="text-overflow m-0">Your Notifications:</h6>
+                        </div>
+                        @foreach(auth()->user()->unreadNotifications as $notification)
+                        <a href="#!" class="dropdown-item">
+                            <i class="ni ni-single-02"></i>
+                            <ul>
+                                <span>
+                                    <li>ID:{{$notification->data['id']}} - Type:{{$notification->data['type']}}</li>
+
+                                </span>
+                            </ul>
+                        </a>
+                        @endforeach
+                        @if(auth()->user()->unreadNotifications->count())
+                        <div class="dropdown-divider"></div>
+                        <a href="/markAsRead" class="dropdown-item">
+                            <span>Mark all as Read</span>
+                        </a>
+                        @endif
+                    </div>
+
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="media align-items-center">
+                                <div class="media-body  ml-2  d-none d-lg-block">
+                                    <span class="mb-0 text-sm  font-weight-bold">{{ Auth::user()->name }}</span>
+                                </div>
+                            </div>
+                        </a>
+                        <div class="dropdown-menu  dropdown-menu-right ">
+                            <div class="dropdown-header noti-title">
+                                <h6 class="text-overflow m-0">Welcome!</h6>
+                            </div>
+                            <a href="#!" class="dropdown-item">
+                                <i class="ni ni-single-02"></i>
+                                <span>User</span>
+                            </a>
+                            <a href="/admin/passwordReset" class="dropdown-item">
+                                <i class="ni ni-settings-gear-65"></i>
+                                <span>Change my password</span>
+                            </a>
+                            <a href="#!" class="dropdown-item">
+                                <i class="ni ni-calendar-grid-58"></i>
+                                <span>Activity</span>
+                            </a>
+                            <a href="#!" class="dropdown-item">
+                                <i class="ni ni-support-16"></i>
+                                <span>Support</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+
+                                <i class="ni ni-user-run"></i>
+                                {{ __('Logout') }}
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </a>
+                        </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </nav>
+                <div style="background-color:#f0f0f7" class="col-md">
+                    @yield('cont')
+                    <br>
+                </div>
             </div>
         </div>
     </div>
     <br>
-
-    {{-- sticky footer --}}
-    <footer class="footer">
-        <div class="d-flex  bg-light justify-content-end">
-            <br>
-            <a href="#" class="text-secondary mr-2">
-                <i class="fab fa-facebook-square"></i> Facebook |
-            </a>
-            <a href="#" class="text-secondary mr-2">
-                <i class="fab fa-twitter-square"></i> Twitter |
-            </a>
-            <a href="#" class="text-secondary mr-2">
-                <i class="fab fa-instagram"></i> Instagram
-            </a>
-        </div>
-        <div class="d-flex bg-light justify-content-center">
-            <h5 class="text-secondary"><i class="far fa-copyright"></i> 2021 by RFSL - LSF - Ministry of Environment</h5><br>
-        </div>
-        <div class="d-flex bg-light justify-content-center">
-            <h6>All rights reserved</h6>
-        </div>
-
-    </footer>
-
+    <div>
+        {{-- sticky footer --}}
+        <footer class="footer">
+            <div class="d-flex  bg-light justify-content-end">
+                <br>
+                <a href="#" class="text-secondary mr-2">
+                    <i class="fab fa-facebook-square"></i> Facebook |
+                </a>
+                <a href="#" class="text-secondary mr-2">
+                    <i class="fab fa-twitter-square"></i> Twitter |
+                </a>
+                <a href="#" class="text-secondary mr-2">
+                    <i class="fab fa-instagram"></i> Instagram
+                </a>
+            </div>
+            <div class="d-flex bg-light justify-content-center">
+                <h5 class="text-secondary"><i class="far fa-copyright"></i> 2021 by RFSL - LSF - Ministry of Environment</h5><br>
+            </div>
+            <div class="d-flex bg-light justify-content-center">
+                <h6>All rights reserved</h6>
+            </div>
+        </footer>
+    </div>
 </body>
-
 </html>
