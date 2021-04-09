0af12b298d5830bf27e12f26004e6f9f4ac27ee0
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2021-04-09 16:53:39 +0530

    adding and fixing the environment module

---
+++ app/Models/Env_type.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Env_type extends Model
+{
+    use HasFactory;
+    protected $table = 'ecosystems_types';
+
+    // A Eco system  has one type and a type can have many eco systems.
+    public function eco_systems()
+    {
+        return $this->hasMany('App\Models\Env');
+    }
+}
--- modules/Environment/routes/environment.php
+++ modules/Environment/routes/environment.php
@@ -2,44 +2,55 @@
 
 use Environment\Http\Controllers\EnvController;
 use Environment\Http\Controllers\SpeciesController;
+use Environment\Http\Controllers\TypeController;
 
 Route::get('/home', [EnvController::class, 'home'])->name('environment.home');
+//General view of the env module
+Route::get('/generalenv', fn() => view('environment::Envmain'));
+// To collect the eco systems data through a form 
+//Route::get('/createrequest', fn() => view('environment::request'));
 
-Route::get('/generalenv', fn() => view('environment::environment'));
-Route::get('/createrequest', fn() => view('environment::request'));
+Route::get('/createrequest', [EnvController::class, 'loadform']);
 
 
+//Route::get('/newrequest', fn() => view('environment::environmenthome'));
 
-Route::get('/newrequest', fn() => view('environment::environmenthome'));
-Route::get('/newspecies', fn() => view('environment::species'));
 
 //Route::get('/requesteco', fn() => view('environment::request')); --no need of two routes two to link to the same view
-Route::get('/neweco', fn() => view('environment::ecohome'));
+//Route::get('/neweco', fn() => view('environment::ecohome'));
 Route::post('/newrequest', [EnvController::class, 'store']);
 Route::put('/environment/updatestatus/{id}',[EnvController::class, 'statusupdate']);
 
 Route::get('/updatedata', [EnvController::class, 'index']);
-Route::get('/deletedataeco', fn() => view('environment::checkstatuseco'));
 
 
+// To edit the request 
 Route::get('edit', [EnvController::class, 'edit']);
 
-Route::get('/deleterequest', fn() => view('environment::checkstatuseco'));
-
+Route::get('/deleterequest', fn() => view('environment::Envindex'));
+//Delete the request by the authorized party
 Route::delete('delete-request/{id}',[EnvController::class, 'delete']);
 
 
-Route::post('/newspecies',[SpeciesController::class, 'store']);
-Route::get('/trackrequst',[SpeciesController::class, 'track']);
-Route::put('/environmentspe/updatestatus/{id}',[SpeciesController::class, 'statusupdate']);
+
 Route::get('/requestdataeco', fn() => view('environment::trackrequesteco'));
 
 Route::get('/trackrequsteco',[EnvController::class, 'track']);
+//More details button , users can see the details of the request
+Route::get('/moreeco/{id}', [EnvController::class, 'more']);
 
+
+//////////// Species module
+
+// To collect species info through a form 
+Route::get('/requestspecies',[SpeciesController::class, 'form']);
+Route::get('/updatedataspecies', [SpeciesController::class, 'index']); 
 //Route::get('/trackrequesteco', 'EnvController@showRequest');
 Route::get('/updatedataspecies',[SpeciesController::class, 'index']);
 
-Route::get('/requestspecies',[SpeciesController::class, 'form']);
-Route::get('/updatedataspecies', [SpeciesController::class, 'index']); 
-
 Route::delete('delete-requestspecies/{id}',[SpeciesController::class, 'delete']);
+Route::post('/newspecies',[SpeciesController::class, 'store']);
+Route::get('/trackrequst',[SpeciesController::class, 'track']);
+Route::put('/environmentspe/updatestatus/{id}',[SpeciesController::class, 'statusupdate']);
+Route::get('/newspecies', fn() => view('environment::species'));
+Route::get('/morespecies/{id}', [SpeciesController::class, 'more']);
