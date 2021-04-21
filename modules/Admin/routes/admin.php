2cca1b728cc4f9c1416b4a9d8481b5133ddb59dc
Sharuka Perera <sharukaperera98@gmail.com>
2021-04-21 21:35:21 +0530

    access control updates

--- modules/Admin/routes/admin.php
+++ modules/Admin/routes/admin.php
@@ -13,11 +13,6 @@ Route::middleware(['auth'])->group(function () {
 // user/index route will route to the UserController to route based on the user's role  
 Route::get('/index', [UserController::class, 'index'])->name('userIndex'); 
 
-//Role Based Access control management
-Route::get('/roleindex',[AdminController::class, 'index'])->name('roleIndex');
-Route::get('/roleedit/{id}',[AdminController::class, 'roleedit'])->name('roleedit');
-Route::post('/rolePriviledge/{id}',[AdminController::class, 'roleupdate']);
-Route::get('/removeAccess/{id}',[AdminController::class, 'accessremove']);
 
 ///////ADMIN ACTIONS      
 Route::get('/create', [UserController::class, 'create']);      // Open create view.
@@ -41,6 +36,15 @@ Route::get('/more/{id}', [UserController::class, 'more']);
 Route::get('/searchUsers', [UserController::class, 'searchUsers']);
 //search - activate users
 Route::get('/searchSelfRegistered', [UserController::class, 'searchSelfRegistered']);
+
+});
+
+Route::middleware(['access.control:3'])->group(function () {
+//Role Based Access control management
+Route::get('/roleindex',[AdminController::class, 'index'])->name('roleIndex');
+Route::get('/roleedit/{id}',[AdminController::class, 'roleedit'])->name('roleedit');
+Route::post('/rolePriviledge/{id}',[AdminController::class, 'roleupdate']);
+Route::get('/removeAccess/{id}',[AdminController::class, 'accessremove']);
 
 });
 
