90eafa404bfccf7b4ee58aa23a6b736af7b75e38
Sharuka Perera <sharukaperera98@gmail.com>
2021-04-22 08:10:26 +0530

    removed additional message display

--- modules/General/ApprovalItem/views/approval.blade.php
+++ modules/General/ApprovalItem/views/approval.blade.php
@@ -1,16 +1,6 @@
 @extends('general')
 
 @section('general')
-<div class="flex row border-secondary rounded-lg ml-3 justify-content-between">
-    <!-- Sessions to display success or failure -->
-    <span>
-        <h3 class="text-center bg-success text-light">{{session('message')}}</h3>
-    </span>
-    <span>
-        <h3 class="text-center bg-danger text-light">{{session('warning')}}</h3>
-    </span>
-    <span>
-</div>
 <h4>Application Information</h4>
 <hr>
 <div class="container">
