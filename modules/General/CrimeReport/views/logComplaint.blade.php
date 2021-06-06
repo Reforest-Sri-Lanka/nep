0be0f2c81dca5f4530ce7a0d303c63734cf5cbb3
Sharuka Perera <sharukaperera98@gmail.com>
2021-06-06 21:28:59 +0530

    adding full map view

--- modules/General/CrimeReport/views/logComplaint.blade.php
+++ modules/General/CrimeReport/views/logComplaint.blade.php
@@ -287,7 +287,7 @@
         ///Converting your layer to a KML
         //$('#kml').val(tokml(drawnItems.toGeoJSON()));
     });
-
+    map.addControl(new L.Control.Fullscreen());
     //SEARCH FUNCTIONALITY
     var searchControl = new L.esri.Controls.Geosearch().addTo(map);
 
