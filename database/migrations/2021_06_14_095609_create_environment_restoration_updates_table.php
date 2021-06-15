f54a1c50f4ac3bd0a16da3fea173dc6d985cc909
Sharuka Perera <sharukaperera98@gmail.com>
2021-06-15 19:11:53 +0530

    environment update migrations

--- database/migrations/2021_06_14_095609_create_environment_restoration_updates_table.php
+++ database/migrations/2021_06_14_095609_create_environment_restoration_updates_table.php
@@ -16,15 +16,19 @@ class CreateEnvironmentRestorationUpdatesTable extends Migration
         Schema::create('environment_restoration_updates', function (Blueprint $table) {
             $table->id();
             $table->string('situation_update');
-            $table->double('suggestions');
-            $table->string('further_remarks');
+            $table->string('suggestions')->nullable();
+            $table->string('further_remarks')->nullable();
+            $table->json('photos');
+            $table->tinyInteger('status');
             $table->timestamps();
 
             //connecting environment restoration species table to environment restorations
             $table->unsignedBigInteger('env_rest_id');
-            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('reviewed_by')->nullable();
             $table->foreign('env_rest_id','er_update')->references('id')->on('environment_restorations')->onDelete('cascade');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('reviewed_by')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
--- database/migrations/2021_06_14_100427_create_environment_restoration_species_updates_table.php
+++ database/migrations/2021_06_14_100427_create_environment_restoration_species_updates_table.php
@@ -15,18 +15,17 @@ class CreateEnvironmentRestorationSpeciesUpdatesTable extends Migration
     {
         Schema::create('environment_restoration_species_updates', function (Blueprint $table) {
             $table->id();
-            $table->tinyInteger('status');
-            $table->string('current_height');
-            $table->string('improvement_suggestions');
-            $table->double('qty_of_successful_trees');
-            $table->string('futher_remarks');
-            
+            $table->double('current_height');
+            $table->string('height_unit')->nullable();
+            $table->string('improvement_suggestions')->nullable();
+            $table->double('qty_of_successful_trees')->nullable();
+            $table->string('futher_remarks')->nullable();
+            $table->timestamps();
+
             $table->unsignedBigInteger('env_rest_update_id');
             $table->unsignedBigInteger('env_rest_species_id')->nullable();
-            $table->unsignedBigInteger('species_id')->nullable();
             $table->foreign('env_rest_update_id','er_update_er')->references('id')->on('environment_restoration_updates')->onDelete('cascade');
             $table->foreign('env_rest_species_id','er_spcs_update_spcs')->references('id')->on('environment_restoration_species')->onDelete('cascade');
-            $table->foreign('species_id')->references('id')->on('species_information')->onDelete('cascade');
         });
     }
 
